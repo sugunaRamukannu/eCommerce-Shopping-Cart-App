06f05e097f473111624e725f9f47b7e07bc4292c
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2025-04-19 22:06:46 +0800

    merge pls
    merge pls

--- src/main/java/sg/nus/iss/service/ecommerceapp/model/Customer.java
+++ src/main/java/sg/nus/iss/service/ecommerceapp/model/Customer.java
@@ -14,7 +14,6 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 
 @Entity
